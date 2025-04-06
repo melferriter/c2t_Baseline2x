30da78853cb6ea1a8f449fbf24e1fa9e2c06e9f3
mferriter <mferriter@ucla.edu>
2025-04-06 14:25:41 -0700

    non customized cloud2trees

---
+++ baseline2x_wade_git.R
@@ -0,0 +1,105 @@
+## pkgbuild helps us check for Rtools
+install.packages("pkgbuild")
+# check for Rtools which is required to build packages
+pkgbuild::check_build_tools(debug = TRUE)
+## remotes helps us get packages hosted on github
+install.packages("remotes")
+## install lasR from the r-univers
+install.packages("lasR", repos = "https://r-lidar.r-universe.dev")
+## install TreeLS from github
+remotes::install_github(repo = "tiagodc/TreeLS", upgrade = F)
+## get cloud2trees
+remotes::install_github(repo = "georgewoolsey/cloud2trees", upgrade = F)
+
+
+# download the external data
+cloud2trees::get_data()
+# download the TreeMap data
+cloud2trees::get_treemap()
+# download the forest type data
+cloud2trees::get_foresttype()
+# download the landfire cbd data
+cloud2trees::get_landfire()
+
+
+# Libraries====
+library(lidR)
+library(sf)
+library(terra)
+library(stars)
+library(raster)
+library(alphashape3d)
+library(plyr)
+library(tidyverse)
+library(devtools)
+library(canopyLazR)
+library(spdep)
+library(sp)
+library(geosphere)
+library(rlas)
+library(rgl)
+library(pracma)
+library(spatstat)
+library(terra)
+library(cloud2trees)
+library(tidyverse)
+library(sf)
+library(purrr)
+library(patchwork)
+library(viridis)
+
+
+rm(list = ls(globalenv()))
+
+# working directory====
+setwd("E:/Grad School/Data/UAS/Sequoia_National_Forest/2024/2024101222_processed/")
+
+
+----------------------------------------------
+  # Extract Trees from Point Cloud: Default====
+----------------------------------------------
+  
+las <- readLAS("E:/Grad School/Data/UAS/Sequoia_National_Forest/2024/2024101222_processed/Agisoft/2x/2xBaseline_20241021232243_clip.las", filter = "-set_withheld_flag 0")
+i <- "E:/Grad School/Data/UAS/Sequoia_National_Forest/2024/2024101222_processed/Agisoft/2x/2xBaseline_20241021232243_clip.las"
+
+# run cloud2trees without customization
+cloud2trees_ans <- cloud2trees::cloud2trees(output_dir = tempdir(), input_las_dir = "E:/Grad School/Data/UAS/Sequoia_National_Forest/2024/2024101222_processed/Agisoft/2x/2xBaseline_20241021232243_clip.las")
+
+# return from cloud2trees package
+cloud2trees_ans %>% names()
+
+# DTM
+cloud2trees_ans$dtm_rast %>% terra::plot()
+
+# CHM
+cloud2trees_ans$chm_rast %>% terra::plot()
+
+# tree crowns (spatial data frame)
+cloud2trees_ans$crowns_sf %>% dplyr::glimpse()
+
+# plot tree crown polygons
+cloud2trees_ans$crowns_sf %>% 
+  ggplot2::ggplot(mapping = ggplot2::aes(fill = tree_height_m)) + 
+  ggplot2::geom_sf() + 
+  ggplot2::scale_fill_distiller(palette = "Oranges", name = "tree ht. (m)", direction = 1) +
+  ggplot2::theme_void() +
+  ggplot2::theme(legend.position = "top", legend.direction = "horizontal")
+
+# tree top points
+cloud2trees_ans$treetops_sf %>% dplyr::glimpse()
+
+# plot tree top points
+cloud2trees_ans$treetops_sf %>% 
+  ggplot2::ggplot(mapping = ggplot2::aes(color = tree_height_m)) + 
+  ggplot2::geom_sf() + 
+  ggplot2::scale_color_distiller(palette = "Oranges", name = "tree ht. (m)", direction = 1) +
+  ggplot2::theme_void() +
+  ggplot2::theme(legend.position = "top", legend.direction = "horizontal")
+
+# plot tree top points on top of tree crowns 
+ggplot2::ggplot() + 
+  ggplot2::geom_sf(data = cloud2trees_ans$crowns_sf, mapping = ggplot2::aes(fill = tree_height_m)) + 
+  ggplot2::geom_sf(data = cloud2trees_ans$treetops_sf, shape = 20) + 
+  ggplot2::scale_fill_distiller(palette = "Oranges", name = "tree ht. (m)", direction = 1) +
+  ggplot2::theme_void() +
+  ggplot2::theme(legend.position = "top", legend.direction = "horizontal")
